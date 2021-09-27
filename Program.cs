40d2a23af8962348e78f6afb98078117e1e41adb
sdmkhan123 <sdmkhan126410@gmail.com>
2021-09-27 14:23:29 +0530

    To check first name is valid or not

--- Program.cs
+++ Program.cs
@@ -6,7 +6,14 @@ namespace REGEXRegularExpressionsAndPattern
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("-----------------------------------------------------------------");
             Console.WriteLine("Welcome to REGEX Regular Expressions & Pattern");
+            Console.WriteLine("-----------------------------------------------------------------");
+            Console.WriteLine("\n***************************************************************************\n");
+            Console.Write("Enter the Frist-Name : ");
+            string fName = Console.ReadLine();
+            Console.WriteLine("First name of the user is : " + ToCheckRegexPattern.validName(fName));
+            Console.WriteLine("\n***************************************************************************\n");
         }
     }
 }
---
+++ ToCheckRegexPattern.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace REGEXRegularExpressionsAndPattern
+{
+    public class ToCheckRegexPattern
+    {
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        /// <summary>
+        /// Function to check name is valid or not
+        /// </summary>
+        /// <param name="fName"></param>
+        public static bool validName(string name)
+        {
+            string namePattern = @"^[A-Z]{1}[a-z]{3,}";
+            //if name pattern is match
+            return (Regex.IsMatch(name, namePattern));
+        }
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    }
+}
