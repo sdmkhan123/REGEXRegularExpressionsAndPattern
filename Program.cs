1f10489d565030f2c85c6bc03df49872b7323cf7
sdmkhan123 <sdmkhan126410@gmail.com>
2021-09-27 14:54:49 +0530

    To check first name is valid or not

--- Program.cs
+++ Program.cs
@@ -12,10 +12,6 @@ namespace REGEXRegularExpressionsAndPattern
             Console.WriteLine("\n***************************************************************************\n");
             ToCheckRegexPattern.validName();
             Console.WriteLine("\n***************************************************************************\n");
-            Console.Write("Enter the Last-Name : ");
-            string lName = Console.ReadLine();
-            Console.WriteLine("Last name of the user is : " + ToCheckRegexPattern.validName(lName));
-            Console.WriteLine("\n***************************************************************************\n");
         }
     }
 }
