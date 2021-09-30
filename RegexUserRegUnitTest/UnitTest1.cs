02c3e45440730fcd490c70160d1d1e1943b8d80c
sdmkhan123 <sdmkhan126410@gmail.com>
2021-09-30 22:11:23 +0530

    Unit Test case for all Regex Pattern of User Reg

--- RegexUserRegUnitTest/UnitTest1.cs
+++ RegexUserRegUnitTest/UnitTest1.cs
@@ -1,18 +1,57 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using REGEXRegularExpressionsAndPattern;
-namespace RegexUserRegUnitTest
+
+namespace RegexUserRegUnitTest.REGEXRegularExpressionsAndPattern
 {
     [TestClass]
     public class UnitTest1
     {
-        ToCheckRegexPattern user = new ToCheckRegexPattern();
         [TestMethod]
         public void FirstNameTest()
         {
             string name = "Saddam";
-            bool result = user.validFirstName();
+            bool result = ToCheckRegexPattern.validFirstName(name);
             Assert.AreEqual(true, result);
-
+        }
+        [TestMethod]
+        public void LastNameTest()
+        {
+            string name = "Khan";
+            bool result = ToCheckRegexPattern.validLastName(name);
+            Assert.AreEqual(true, result);
+        }
+        [TestMethod]
+        public void MobileTest()
+        {
+            string mobile = "91 8965265302";
+            bool result = ToCheckRegexPattern.validMobileNumber(mobile);
+            Assert.AreEqual(true, result);
+        }
+        [TestMethod]
+        public void PasswordTest()
+        {
+            string password = "Sdmkha12@";
+            bool result = ToCheckRegexPattern.validPassword(password);
+            Assert.AreEqual(true, result);
+        }
+        [TestMethod]
+        public void EmailTest()
+        {
+            string email1 = "ahmadiqbal.a786@gmail.com";
+            string email2 = "iqbal143@yahoo.com";
+            string email3 = "abc.200@yahoo.com";
+            string email4 = "abc-567@abc.com";
+            string email5 = "abc200@abc.co.in";
+            bool result = ToCheckRegexPattern.ValidateEmailAllRules(email1);
+            bool result3 = ToCheckRegexPattern.ValidateEmailAllRules(email2);
+            bool result4 = ToCheckRegexPattern.ValidateEmailAllRules(email3);
+            bool result5 = ToCheckRegexPattern.ValidateEmailAllRules(email4);
+            bool result2 = ToCheckRegexPattern.ValidateEmailAllRules(email5);
+            Assert.AreEqual(true, result);
+            Assert.AreEqual(true, result2);
+            Assert.AreEqual(true, result3);
+            Assert.AreEqual(true, result4);
+            Assert.AreEqual(true, result5);
         }
     }
 }
